299b96b4b7976ef416bcacbec9086d93bb13cc79
zhx_hexo <zhx_silentheart@163.com>
2019-06-12 23:06:48 +0800

    首页
    顶部tan栏，底部菜单

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,27 +2,36 @@
 import Page from '../../helpers/wxPage.js'
 
 Page({
-    data: {
-
-    },
-    onLoad: function(options) {
-        let data = this.router.extract(options)
-        console.log(data)
-        console.log(options.c)
-        console.log(options.d)
-        console.log(this.event)
-        this.event.on('DataChanged', this, function(data) {
-            this.setData({
-                motto: data
-            });
-        })
-    },
-    onTap(){
-      this.router.push({
-          name:"test"
+  data: {
+    nowIdx: "0",
+    topTar: ['用户发布','活动新闻']
+  },
+  onLoad: function(options) {
+      let data = this.router.extract(options)
+      console.log(data)
+      console.log(options.c)
+      console.log(options.d)
+      console.log(this.event)
+      this.event.on('DataChanged', this, function(data) {
+          this.setData({
+              motto: data
+          });
       })
-    },
-    onUnload: function() {
-        this.event.remove('DataChanged', this);
-    }
+  },
+  onTap(){
+    this.router.push({
+        name:"test"
+    })
+  },
+  onUnload: function() {
+      this.event.remove('DataChanged', this);
+  },
+  tarClick(e) {
+    console.log(e)
+    let id = e.currentTarget.dataset.nowid;
+    console.log(id)
+    this.setData({
+      nowIdx: id
+    })
+  }
 })
