63168c8f85bd338c89cfe591fd76d83835f30b36
前端学习 <zhx_silentheart@163.com>
2019-07-13 23:27:19 +0800

    优化界面
    修改首页增加争鸣及其个人中心

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,15 +4,56 @@ import { getDateTimeStamp, getDateDiff } from "../../utils/mp-router/time.js";
 Page({
   data: {
     nowIdx: 0,
-    topTar: ['用户发布','活动新闻'],
+    topTar: ['校园圈','校园新闻'],
     listNews: [],
     pushlist: [],
     imgUrl: app.indexApi.ImgUrl,
+    cardCur: 0,
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563040353902&di=e19dcb234d885660ea2788d678f0242a&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2FM7HHGCYwSh9HSbjALBJKlg%3D%3D%2F1147010530113851225.jpg'
+    }, {
+      id: 1,
+      type: 'image',
+        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563040353902&di=e19dcb234d885660ea2788d678f0242a&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2FM7HHGCYwSh9HSbjALBJKlg%3D%3D%2F1147010530113851225.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563040353902&di=e19dcb234d885660ea2788d678f0242a&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2FM7HHGCYwSh9HSbjALBJKlg%3D%3D%2F1147010530113851225.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1563040353902&di=e19dcb234d885660ea2788d678f0242a&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2FM7HHGCYwSh9HSbjALBJKlg%3D%3D%2F1147010530113851225.jpg'
+    }],
+    iconList: [{
+      icon: 'cardboardfill',
+      color: 'red',
+      badge: 120,
+      name: '板块一'
+    }, {
+      icon: 'recordfill',
+      color: 'orange',
+      badge: 1,
+      name: '板块二'
+    }, {
+      icon: 'picfill',
+      color: 'yellow',
+      badge: 0,
+      name: '板块三'
+    }],
+    gridCol: 3,
+    skin: false
   },
   onLoad: function(options) {
     const that = this;
     that.loadNewsList();
     that.loadUsreList();
+  },
+  cardSwiper(e) {
+    this.setData({
+      cardCur: e.detail.current
+    })
   },
   // 加载新闻列表
   loadNewsList: function() {
@@ -65,6 +106,11 @@ Page({
     let userid = e.currentTarget.dataset.userid;
     wx.navigateTo({
       url: './pushdetail/pushdetail?id=' + userid
+    })
+  },
+  goadd() {
+    wx.navigateTo({
+      url: '/pages/add/add'
     })
   }
 })
